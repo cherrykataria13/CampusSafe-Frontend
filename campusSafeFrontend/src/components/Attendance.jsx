e0680c97a767210533813d9c16548a07a31389cc
Rahul <rahulguglani75000@gmail.com>
2024-05-26 00:46:24 +0530

    attendance with incorrect css

--- campusSafeFrontend/src/components/Attendance.jsx
+++ campusSafeFrontend/src/components/Attendance.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import Topbar from "../Topbar";
 const Attendance = () => {
   const { studentId, subjectId } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [attendanceData, setAttendanceData] = useState(null);
-
+  const [subjectName,setSubjectName] = useState(null);
   useEffect(() => {
     fetch(`http://localhost:8080/dashboard/${studentId}/subjects/${subjectId}/attendance`, {
       method: 'GET',
@@ -18,6 +18,7 @@ const Attendance = () => {
       .then((response) => response.json())
       .then((data) => {
         setAttendanceData(data.data);
+        setSubjectName(data.subName);
         setLoading(false);
       })
       .catch((error) => {
@@ -36,14 +37,32 @@ const Attendance = () => {
 
   return (
     <div>
-      <h2>Attendance for Subject: {subjectName}</h2>
-      <ul>
-        {attendanceData.map((record, index) => (
-          <li key={index}>
-            Date: {record.attendance_date}, Status: {record.status}
-          </li>
+      <Topbar />
+    <div className="attendance-table-container">
+      <h2 id="attendance-label">Attendance for Subject: {subjectName}</h2>
+      <table className="attendance-table">
+      <thead>
+        <tr>
+          <th>Lecture ID</th>
+          <th>Date</th>
+          <th>Details</th>
+          <th>Location</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {attendanceData.map((lecture) => (
+          <tr key={lecture.lecture_id}>
+            <td>{lecture.lecture_id}</td>
+            <td>{new Date(lecture.lecture_date).toLocaleDateString()}</td>
+            <td>{lecture.lecture_details}</td>
+            <td>{lecture.loc}</td>
+            <td>{lecture.status}</td>
+          </tr>
         ))}
-      </ul>
+      </tbody>
+    </table>
+    </div>
     </div>
   );
 };
